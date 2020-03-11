8ea2a41f61b2b18b83893a6aa58bd899ea3294d8
Sam <sammygutierrez335@gmail.com>
2020-03-11 15:25:43 -0500

    adds moving enemy

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-import Spaceship from "./Spaceship.js"
+import Spaceship from "./spaceship.js"
+import Enemy from "./enemy.js"
 
 
 // Creates the canvas
@@ -9,8 +10,8 @@ canvas.height = 480;
 document.body.appendChild(canvas);
 
 let audio = new Audio("sound/Space Ambience.mp3")
-audio.play()
-audio.pause()
+// audio.play()
+// audio.pause()
 
 
 
@@ -29,16 +30,26 @@ bgImage.onload = function () {
 
 //player spaceship
 let spaceShip = new Spaceship({
-  speed: 3,
+  speed: 4,
   x: 50,
   y: 200,
   imgSrc: "assets/player/playership.png",
 })
 
+let enemy = new Enemy({
+  speed: 2,
+  x: 1000,
+  y: 200,
+  imgSrc: "assets/attackers/atom.png"
+})
+
 let spaceShipImage = new Image();
 spaceShip.renderImg(spaceShipImage)
-window.requestAnimationFrame(gameLoop)
 
+let enemyImage = new Image()
+enemy.renderImg(enemyImage)
+
+window.requestAnimationFrame(gameLoop)
 
 
 
@@ -62,17 +73,21 @@ const HEIGHT = 64
 const SCALE = 1
 const SCALED_WIDTH = SCALE * WIDTH
 const SCALED_HEIGHT = SCALE * HEIGHT
+
+
 //(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 //draws a sprite frame dynamically - sprites are 64x64pixels
-
-function drawFrame(frameX, frameY, canvasX, canvasY) {
+function drawFrame(frameX, frameY, canvasX, canvasY, enemyX, enemyY) {
   ctx.drawImage(bgImage, 0, 0)
   ctx.drawImage(spaceShipImage,
     frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
     canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
+  ctx.drawImage(enemyImage,
+    frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
+    enemyX, enemyY, SCALED_WIDTH, SCALED_HEIGHT);
 }
 
-
+//enemy atom has 24 stack frames
 const CYCLE_LOOP = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let currentLoopIndex = 0;
 let frameCount = 0;
@@ -101,6 +116,7 @@ function gameLoop() {
     // moveShip(spaceShip.speed, 0, FLYING_RIGHT)
   }
 
+  enemy.moveEnemy(enemy.speed, 0, 0, canvas)
   frameCount++;
   if (frameCount >= FRAME_LIMIT) {
     frameCount = 0;
@@ -110,7 +126,6 @@ function gameLoop() {
     }
   }
 
-
-  drawFrame(CYCLE_LOOP[currentLoopIndex], 0, spaceShip.x, spaceShip.y);
+  drawFrame(CYCLE_LOOP[currentLoopIndex], 0, spaceShip.x, spaceShip.y, enemy.x, enemy.y);
   window.requestAnimationFrame(gameLoop);
 }
--- js/enemy.js
+++ js/enemy.js
@@ -1,4 +1,4 @@
-export default class Spaceship {
+export default class Enemy {
   constructor(options) {
     this.speed = options.speed; // movement in pixels per second
     this.x = options.x;
@@ -6,15 +6,16 @@ export default class Spaceship {
     this.imgSrc = options.imgSrc
   }
 
-  renderImg(spaceShipImage) {
-    spaceShipImage.src = this.imgSrc;
+  renderImg(enemyImage) {
+    enemyImage.src = this.imgSrc;
   }
-  moveShip(deltaX, deltaY, direction, canvas) {
-    if (this.x + deltaX > 0 && this.x + 64 + deltaX < canvas.width) {
-      this.x += deltaX;
+  //checks for inbounds
+  moveEnemy(deltaX, deltaY, direction, canvas) {
+    if (this.x + deltaX > 0) {
+      this.x -= deltaX;
     }
     if (this.y + deltaY > 0 && this.y + 64 + deltaY < canvas.height) {
-      this.y += deltaY;
+      this.y -= deltaY;
     }
   }
 }
