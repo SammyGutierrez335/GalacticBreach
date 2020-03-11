de2ac8e8015e89233abe5072599b3a1f0d245812
Sam <sammygutierrez335@gmail.com>
2020-03-11 00:21:43 -0500

    adds bounds

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
-// Create the canvas
+// Creates the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 960;
 canvas.height = 480;
 document.body.appendChild(canvas);
+
+
+
 
 
 
@@ -16,83 +19,116 @@ bgImage.onload = function () {
   bgReady = true;
 };
 
+
+
+
+
+//player spaceship
 var spaceShipImage = new Image();
 spaceShipImage.src = "assets/player/playership.png";
 var ssReady = false
 spaceShipImage.onload = function () {
   ssReady = true
-  render()
+
+  window.requestAnimationFrame(gameLoop);
+
 }
 
-
+var spaceShip = {
+  speed: 3, // movement in pixels per second
+  x: 50,
+  y: 200
+};
 
 // Handle keyboard controls
 //To accomplish this we simply have a variable keysDown which 
 //stores any event's keyCode. If a key code is in the object, the user is currently pressing that key. Simple!
-var keysDown = {};
+let keyPresses = {};
 
-addEventListener("keydown", function (e) {
-  keysDown[e.keyCode] = true;
-}, false);
+window.addEventListener('keydown', keyDownListener, false);
+function keyDownListener(event) {
+  keyPresses[event.key] = true;
+}
 
-addEventListener("keyup", function (e) {
-  delete keysDown[e.keyCode];
-}, false);
+window.addEventListener('keyup', keyUpListener, false);
+function keyUpListener(event) {
+  keyPresses[event.key] = false;
+}
 
 
 
-let width = 64
-let height = 64
-let scale = .7
-let scaledWidth = scale * width
-let scaledHeight = scale * height
+
+
+
+
+const WIDTH = 64
+const HEIGHT = 64
+const SCALE = 1
+const SCALED_WIDTH = SCALE * WIDTH
+const SCALED_HEIGHT = SCALE * HEIGHT
 //(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 //draws a sprite frame dynamically - sprites are 64x64pixels
-//frame0 -
+
 function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(bgImage, 0, 0)
   ctx.drawImage(spaceShipImage,
-    frameX * width, frameY * height, width, height,
-    canvasX, canvasY, scaledWidth, scaledHeight);
+    frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
+    canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
 }
 
 
-const cycleLoop = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const CYCLE_LOOP = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 let currentLoopIndex = 0;
 let frameCount = 0;
+const FLYING_DOWN = 0;
+const FLYING_UP = 0;
+const FLYING_LEFT = 0;
+const FLYING_RIGHT = 0;
+let currentDirection = FLYING_DOWN;
 
-function step() {
-  frameCount++
-  if (frameCount < 4) {
-    window.requestAnimationFrame(step);
-    return
-  }
-  frameCount = 0
+const FRAME_LIMIT = 5;
+// The main game loop
+function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(bgImage, 0, 0)
-  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
-  currentLoopIndex++;
-  if (currentLoopIndex >= cycleLoop.length) {
-    currentLoopIndex = 0;
+
+  let hasMoved = false;
+
+  if (keyPresses.w) {
+    moveShip(0, -spaceShip.speed, FLYING_UP)
+    hasMoved = true;
+  } else if (keyPresses.s) {
+    moveShip(0, spaceShip.speed, FLYING_DOWN)
+    hasMoved = true;
   }
-  window.requestAnimationFrame(step);
+  if (keyPresses.a) {
+    moveShip(-spaceShip.speed, 0, FLYING_LEFT)
+    hasMoved = true;
+  } else if (keyPresses.d) {
+    moveShip(spaceShip.speed, 0, FLYING_RIGHT)
+    hasMoved = true;
+  }
+
+  frameCount++;
+  if (frameCount >= FRAME_LIMIT) {
+    frameCount = 0;
+    currentLoopIndex++;
+    if (currentLoopIndex >= CYCLE_LOOP.length) {
+      currentLoopIndex = 0;
+    }
+  }
+
+
+  drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, spaceShip.x, spaceShip.y);
+  window.requestAnimationFrame(gameLoop);
 }
 
+function moveShip(deltaX, deltaY, direction) {
+  if (spaceShip.x + deltaX > 0 && spaceShip.x + SCALED_WIDTH + deltaX < canvas.width) {
+    spaceShip.x += deltaX;
+  }
+  if (spaceShip.y + deltaY > 0 && spaceShip.y + SCALED_HEIGHT + deltaY < canvas.height) {
+    spaceShip.y += deltaY;
+  }
+  currentDirection = direction;
+}
 
-// The main game loop
-var main = function () {
-  var now = Date.now();
-  var delta = now - then;
-  ctx.drawImage(bgImage, 0, 0);
-
-
-  then = now;
-
-  // Request to do this again ASAP
-  requestAnimationFrame(step);
-};
-
-
-// Play the game
-var then = Date.now();
-
-main();
